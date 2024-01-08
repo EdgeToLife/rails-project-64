b58f64b3f7ea20e86243eb218d856b53024e99f3
Ivan Zhukov <ziv@servers.com>
2024-01-08 17:48:28 +0200

    clean all and recrate project

--- app/models/post_comment.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class PostComment < ApplicationRecord
-  belongs_to :post
-  belongs_to :user
-  validates :content, presence: true
-  has_ancestry
-end
--- db/migrate/20231228073526_create_post_comments.rb
+++
@@ -1,9 +0,0 @@
-class CreatePostComments < ActiveRecord::Migration[7.1]
-  def change
-    create_table :post_comments do |t|
-      t.text :content
-
-      t.timestamps
-    end
-  end
-end
