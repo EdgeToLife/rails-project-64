ebb759f53b26e211f3252ca6343296eb10ae88df
Ivan Zhukov <ziv@servers.com>
2024-01-15 13:41:01 +0200

    review-1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: 'Пост создан.' }
+        format.html { redirect_to post_url(@post), notice: 'Post was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -40,7 +40,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: 'Пост обновлен.' }
+        format.html { redirect_to post_url(@post), notice: 'Post was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -52,7 +52,7 @@ class PostsController < ApplicationController
     @post.destroy!
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Пост удален.' }
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
     end
   end
 
